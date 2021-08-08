c86782be73a71b9b81287e4b4619f5280cfb7a72
Julia Cooke <juliariec@gmail.com>
2021-08-08 10:27:25 -0400

    Updated 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,8 +2,8 @@ import React from "react"
 import Layout from "../components/Layout"
 
 const Error = () => (
-  <Layout title="404 error" description="Page not found." article={false}>
-    <h1>404 error</h1>
+  <Layout title="Error 404" description="Page not found." article={false}>
+    <h1>Error 404</h1>
     <p>The page you requested could not be found.</p>
   </Layout>
 )
