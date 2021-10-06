86772a85074b513b5647b32505a17de6a0e48aa9
Julia Cooke <juliariec@gmail.com>
2021-10-05 20:43:53 -0400

    Homepage shows 3 posts per category

--- src/components/CategorySnippet.js
+++ src/components/CategorySnippet.js
@@ -6,7 +6,7 @@ const CategorySnippet = ({ category, items }) => {
   return (
     <div className="category">
       <Tag key={`${category}-tag`} category={category} />
-      {items.slice(0, 5).map(({ node }) => (
+      {items.slice(0, 3).map(({ node }) => (
         <p key={node.id}>
           <Link to={node.fields.slug}>{node.frontmatter.title}</Link>
         </p>
