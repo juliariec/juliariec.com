8c3cc3dc494509fcd904333f632276a03aecc006
Julia Cooke <juliariec@gmail.com>
2022-03-23 17:32:11 -0400

    Updated RSS feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -88,8 +88,6 @@ module.exports = {
             serialize: ({ query: { site, allMarkdownRemark } }) => {
               return allMarkdownRemark.edges.map(edge => {
                 return Object.assign({}, edge.node.frontmatter, {
-                  description: edge.node.frontmatter.description,
-                  date: edge.node.frontmatter.date,
                   url: site.siteMetadata.siteUrl + edge.node.fields.slug,
                   guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
                   custom_elements: [{ "content:encoded": edge.node.html }],
@@ -118,6 +116,7 @@ module.exports = {
               `,
             output: "/rss.xml",
             title: "juliariec",
+            author: "Julia Cooke",
           },
         ],
       },
